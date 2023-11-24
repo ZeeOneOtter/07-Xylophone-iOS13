b50f0f79e1e699c186cf82a1954cb4918b0f0b87
ZeeOneOtter <61956050+ZeeOneOtter@users.noreply.github.com>
2023-11-24 13:45:18 -0800

    This allows the user to see what app is pressed by decreasing the opacity of the key pressed for 0.2 seconds.

--- Xylophone/ViewController.swift
+++ Xylophone/ViewController.swift
@@ -20,13 +20,23 @@ class ViewController: UIViewController {
     }
     
     @IBAction func keyPressed(_ sender: UIButton) {
+        //call function to play sound
         playSound(soundName: sender.currentTitle!)
+        
+        //Reduces the sender's (the button that got pressed) opacity to half.
+        sender.alpha = 0.5
+        
+        //Code should execute after 0.2 second delay.
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.2) {
+            //Bring's sender's opacity back up to fully opaque.
+            sender.alpha = 1.0
+        }
+        
+        func playSound(soundName : String) {
+            let soundUrl = Bundle.main.url(forResource: soundName, withExtension: "wav")
+            audioPlayer = try! AVAudioPlayer(contentsOf: soundUrl!)
+            audioPlayer.play()
+        }
+        
     }
-    
-    func playSound(soundName : String) {
-        let soundUrl = Bundle.main.url(forResource: soundName, withExtension: "wav")
-        audioPlayer = try! AVAudioPlayer(contentsOf: soundUrl!)
-        audioPlayer.play()
-    }
-    
 }
